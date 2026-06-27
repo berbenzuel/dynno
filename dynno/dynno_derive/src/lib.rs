326d064b527ac1f55e940c826db656e1a163cb94
berbenzuel <zukovnik1010@gmail.com>
2026-06-27 18:49:33 +0200

    temp

--- dynno/dynno_derive/src/lib.rs
+++ dynno/dynno_derive/src/lib.rs
@@ -1,8 +1,1 @@
-use proc_macro::TokenStream;
-use std::collections::HashMap;
-use proc_macro2::Span;
-use quote::quote;
-use syn::{parse, parse_macro_input, DeriveInput};
-use proc_macro_crate::{crate_name, FoundCrate};
-use uuid::Uuid;
 
